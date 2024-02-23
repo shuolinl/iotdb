2c6634103292cfc1fb6474f413dd577bd6984c16
shuolin_l <shuolin_l@163.com>
2024-02-23 18:22:17 +0800

    tmp2

--- iotdb-core/datanode/src/main/java/org/apache/iotdb/db/tools/schema/SchemaRegionSnapshotParser.java
+++ iotdb-core/datanode/src/main/java/org/apache/iotdb/db/tools/schema/SchemaRegionSnapshotParser.java
@@ -1,14 +1,23 @@
  package org.apache.iotdb.db.tools.schema;
 
 
+ import com.sun.jdi.ArrayReference;
  import org.apache.iotdb.commons.path.PartialPath;
+ import org.apache.iotdb.commons.schema.SchemaConstant;
+ import org.apache.iotdb.consensus.config.RatisConfig;
  import org.apache.iotdb.db.queryengine.plan.statement.Statement;
+ import org.apache.iotdb.db.schemaengine.schemaregion.mtree.impl.mem.mnode.IMemMNode;
+ import org.apache.iotdb.db.schemaengine.schemaregion.mtree.impl.mem.mnode.impl.BasicInternalMNode;
+ import org.apache.iotdb.tsfile.utils.ReadWriteIOUtils;
 
- import java.io.File;
- import java.nio.file.Files;
+ import java.io.*;
  import java.nio.file.Path;
+ import java.util.ArrayDeque;
  import java.util.List;
  import java.util.Collections;
+ import java.util.Deque;
+
+ import static org.apache.iotdb.commons.schema.SchemaConstant.*;
 
  public class SchemaRegionSnapshotParser  {
      /**
@@ -33,21 +42,79 @@
         if (!snapshotFile.exists()) {
             return null;
         }
-    }
 
-    private class StatementIterator implements java.util.Iterator<Statement> {
-        public boolean hasNext() {
-            return false;
+        if (!snapshotFile.getName().equals(SchemaConstant.MTREE_SNAPSHOT)) {
+            throw new IllegalArgumentException(String.format("%s is not allowed, only support %s", snapshotFile.getName(), SchemaConstant.MTREE_SNAPSHOT));
         }
 
-        public Statement next() {
-            return new Statement() {
-                @Override
-                public List<PartialPath> getPaths() {
-                    return null;
-                }
-            };
+        // 这里只假设我们获取了  MemTree 文件，暂时先不考虑 Tag 文件，Tag 文件的解析工作后面再加入。
+        try (BufferedInputStream inputStream = new BufferedInputStream(new FileInputStream(snapshotFile))) {
+            byte version = ReadWriteIOUtils.readByte(inputStream);
+
+
+
+
+
+
+
+        } catch (Exception e) {
+            // not handle temp.
         }
     }
+
+
+    private IMemMNode deserializeMemTree(InputStream inputStream) {
+        Deque<IMemMNode> ancestors = new ArrayDeque<>();
+        Deque<Integer> restChildrenNum = new ArrayDeque<>();
+        deserializeMNode()
+    }
+
+    private static void deserializeMNode(Deque<IMemMNode> ancestors, Deque<Integer> restChildrenNum,
+                                         InputStream inputStream) throws IOException {
+        byte type = ReadWriteIOUtils.readByte(inputStream);
+        IMemMNode node;
+        int childrenNum;
+        switch (type) {
+            case INTERNAL_MNODE_TYPE:
+            case STORAGE_GROUP_MNODE_TYPE:
+                childrenNum = ReadWriteIOUtils.readInt(inputStream);
+                String name = ReadWriteIOUtils.readString(inputStream);
+                node = new BasicInternalMNode(null, name);
+                ReadWriteIOUtils.readInt(inputStream);
+                ReadWriteIOUtils.readBool(inputStream);
+                break;
+            case ENTITY_MNODE_TYPE:
+            case STORAGE_GROUP_ENTITY_MNODE_TYPE:
+                childrenNum = ReadWriteIOUtils.readInt(inputStream);
+                node  = deserializeInternalMNode(inputStream);
+                int templateid = ReadWriteIOUtils.readInt(inputStream);
+                boolean useTemplate = ReadWriteIOUtils.readBool(inputStream);
+                boolean isAligned = ReadWriteIOUtils.readBoolObject(inputStream);
+                break;
+
+
+
+
+
+        }
+
+
+    }
+
+    private static IMemMNode deserializeInternalMNode(InputStream inputStream) throws IOException {
+        String name = ReadWriteIOUtils.readString(inputStream);
+        IMemMNode node = new BasicInternalMNode(null, name);
+        ReadWriteIOUtils.readInt(inputStream);
+        ReadWriteIOUtils.readBool(inputStream);
+        return node;
+    }
+
+    private static IMemMNode deserializeStorageGroupMNode(InputStream inputStream) throws IOException {
+        String name = ReadWriteIOUtils.readString(inputStream);
+        IMemMNode node = new Basic
+    }
+
+
+
 
  }
